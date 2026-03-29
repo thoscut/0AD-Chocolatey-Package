600286fec6e37358707bfe10fdb35f718111419d
thoscut <thomas.scheel@gmx.net>
2026-03-29 13:02:03 +0200

    Update 0ad installer to version 0.28.0

--- 0ad/tools/chocolateyInstall.ps1
+++ 0ad/tools/chocolateyInstall.ps1
@@ -1,16 +1,21 @@
-$packageName = '0ad'
-$installerType = 'exe'
-$silentArgs = '/S'
-$fileName = '0ad-0.27.0-win32.exe'
-$url = "https://releases.wildfiregames.com/$fileName"
-$checksum = 'f886b5cc76a11163f19628be4619a1facd771c94'
-$checksumType = 'sha1'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+$packageArgs = @{
+  packageName    = '0ad'
+  softwareName   = '0.A.D.'
+  fileType       = 'exe'
+
+  fileName       = '0ad-0.28.0-win32.exe'
+  fileName64     = '0ad-0.28.0-win64.exe'
+  url           = "https://releases.wildfiregames.com/$fileName"
+  checksum       = 'e2b30d28a80ea5cd9c07dce025b5c17a96f02dd1443f6802966876b2aa56642f'
+  checksumType   = 'sha256'
+  url64           = "https://releases.wildfiregames.com/$fileName64"
+  checksum64     = '9b5e1a16d4e0df9c84e052f56fb75da69518d5634c88d2b38db69baee63c16a1'
+  checksumType64 = 'sha256'
+
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
